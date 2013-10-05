f49de24373c776a0df2ed20b6b250a954a033fd4
Mike Ebert <mike@mikeebert.net>
2013-10-05 10:46:40 -0500

    Adds goodbye method

--- hello_osmoces.rb
+++ hello_osmoces.rb
@@ -2,6 +2,11 @@ class HelloOsmoces
   def self.say_hello
     puts "Hello everyone!"
   end
+
+  def self.say_goodbye
+    puts "Goodbye"
+  end
 end
 
 HelloOsmoces.say_hello
+HelloOsmoces.say_goodbye
